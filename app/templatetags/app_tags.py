0122c6b585250262daaa4d454ad2d33ef63f7207
Christian Wiegand <christianwgd@users.noreply.github.com>
2023-07-10 19:08:08 +0200

    Fix error if no metrics for app

--- app/templatetags/app_tags.py
+++ app/templatetags/app_tags.py
@@ -26,23 +26,25 @@ def health_check_color(value):
 
 @register.simple_tag
 def metrics_color(app, value_type):
-    value = getattr(app.metrics.latest('timestamp'), value_type)
-    threshold_name = f'max_{value_type}'
-    if hasattr(app, threshold_name):
-        threshold = getattr(app, threshold_name)
-        if value > threshold:
-            return 'danger'
-        return 'success'
+    if app.metrics.count() > 0:
+        value = getattr(app.metrics.latest('timestamp'), value_type)
+        threshold_name = f'max_{value_type}'
+        if hasattr(app, threshold_name) :
+            threshold = getattr(app, threshold_name)
+            if value > threshold:
+                return 'danger'
+            return 'success'
     return 'default'
 
 
 @register.simple_tag
 def metrics_icon(app, value_type):
-    value = getattr(app.metrics.latest('timestamp'), value_type)
-    threshold_name = f'max_{value_type}'
-    if hasattr(app, threshold_name):
-        threshold = getattr(app, threshold_name)
-        if value > threshold:
-            return 'exclamation-circle-fill'
-        return 'check-circle-fill'
+    if app.metrics.count() > 0:
+        value = getattr(app.metrics.latest('timestamp'), value_type)
+        threshold_name = f'max_{value_type}'
+        if hasattr(app, threshold_name):
+            threshold = getattr(app, threshold_name)
+            if value > threshold:
+                return 'exclamation-circle-fill'
+            return 'check-circle-fill'
     return None
