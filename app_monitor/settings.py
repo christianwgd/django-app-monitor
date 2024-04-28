bce65be2350e9822916e0d4ae499afa6c80f1b94
Christian Wiegand <christianwgd@users.noreply.github.com>
2024-04-28 14:50:47 +0200

    Provide dark mode

--- app_monitor/settings.py
+++ app_monitor/settings.py
@@ -48,8 +48,8 @@ INSTALLED_APPS = [
     'app',
     'app_monitor',
     'chartjs',
-    'django_bootstrap5',
     'django_bootstrap_icons',
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -132,6 +132,23 @@ LOGOUT_REDIRECT_URL = reverse_lazy('home')
 
 # bootstrap-icons
 BS_ICONS_CACHE = os.path.join(STATIC_ROOT, 'icon_cache')
+
+###############
+# BOOTSTRAP 5 #
+###############
+BOOTSTRAP5 = {
+    "color_mode": None,
+    "css_url": {
+        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+        "integrity": "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH",
+        "crossorigin": "anonymous",
+    },
+    "javascript_url": {
+        "url": "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+        "integrity": "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz",
+        "crossorigin": "anonymous",
+    },
+}
 
 ##################
 # LOCAL SETTINGS #
